fe6826cf239cc7a2aa9b16f802a5ed5f301140e9
Joao Cavalcante <joao_marcos.cs@outlook.com>
2024-10-09 11:19:00 +0200

    Update solve.py

--- mfpy/solve.py
+++ mfpy/solve.py
@@ -8,7 +8,7 @@ def solve(input_values,header,check_limits=False):
 
     PreProcessing.check_input_size(input_values) #Check input len
 
-    output = namedtuple('MFOutput',["alpha","kappa",'gamma','VX',"FX","FY",'muX','muY','FZ','MX','MY','MZ','t','s','FX0','FY0','MZ0','MZr0','t0'])
+    output = namedtuple('MFOutput',["alpha","kappa",'gamma','VX',"FX","FY",'muX','muY','FZ','MX','MY','MZ','t','s','FX0','FY0','MZ0','MZr0','t0','Re','rho','omega'])
     
     if check_limits == True:
         input_final = PreProcessing.check_limits(header_checked,input_values) #Limitating the input using the tir file parameter
@@ -17,7 +17,7 @@ def solve(input_values,header,check_limits=False):
     alpha_input,kappa_input,gamma_input,Fz_input,Vx_input = input_values #Initial values (input user)
     alpha,kappa,gamma,Fz,Vx = input_final #Creating solve variables for calculating corrected by lim check
 
-    nominal,Fx_pure_coef,Fy_pure_coef,Mz_pure_coef,Fx_combined_coef,Fy_combined_coef,Mz_combined_coef,My_coef,Mx_coef = PreProcessing.creating_coefs(header_checked) #The necessary coefs for pacejka function
+    nominal,Radius_coef,Fx_pure_coef,Fy_pure_coef,Mz_pure_coef,Fx_combined_coef,Fy_combined_coef,Mz_combined_coef,My_coef,Mx_coef = PreProcessing.creating_coefs(header_checked) #The necessary coefs for pacejka function
 
     Fx_pure_output = Pacejka.Fx_pure((kappa,Fz),*Fx_pure_coef,nominal['FZ0']) #Fx0 pure slip
 
@@ -36,6 +36,7 @@ def solve(input_values,header,check_limits=False):
 
     Mx_output = Pacejka.Mx_overturning((gamma,Fz,Vx),nominal['FZ0'],nominal['R0'],Fy_combined_output,*Mx_coef) #Overturning torque
 
+    Radius_output = Pacejka.Radius((Fz,Vx),*Radius_coef,nominal['FZ0'],nominal['R0'])
 
     result = output(muX=Fx_pure_output[1],FX0=Fx_pure_output[0],
                     muY=Fy_pure_output[1],FY0=Fy_pure_output[0],
@@ -44,7 +45,8 @@ def solve(input_values,header,check_limits=False):
                     MZ=Mz_combined_output[0],s=Mz_pure_output[2],t=Mz_pure_output[1],
                     MY=My_output[0],MX=Mx_output[0],
                     alpha=alpha_input,gamma=gamma_input,FZ=Fz_input,
-                    kappa=kappa_input,VX=Vx_input)
+                    kappa=kappa_input,VX=Vx_input,
+                    Re=Radius_output[0],rho=Radius_output[1],omega=Radius_output[2])
 
     return result
 
